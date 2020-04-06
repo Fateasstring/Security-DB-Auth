ae41cb60ee0cc9dcfa02e7714ebc20cb1ecbd34d
Fateasstring <idarion@qq.com>
2020-04-06 17:00:27 +0800

    first update

---
+++ src/main/java/com/fateasstring/securitydbauth/SecuritydbauthApplication.java
@@ -0,0 +1,13 @@
+package com.fateasstring.securitydbauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritydbauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritydbauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fateasstring/securitydbauth/SecuritydbauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fateasstring.securitydbauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritydbauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
